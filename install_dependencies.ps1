c5a295ebd18790a25e18c1c484e600372bf3c7d7
orlando <75084509+dehoisted@users.noreply.github.com>
2021-08-30 01:18:56 -0400

    Update install_dependencies.ps1

--- install_dependencies.ps1
+++ install_dependencies.ps1
@@ -1,17 +1,10 @@
-#Requires -RunAsAdministrator
+# vcpkg is a C/C++ package manager (docs & install https://vcpkg.io/en/getting-started.html)
+# Current Directory should be vcpkg when running script.
+# Requires -RunAsAdministrator
 
-echo "Installing needed dependencies for CLEO v5 build process!"
-echo "Please hang tight!"
-
-$path = Read-Host -Prompt 'Please enter your VCPKG INSTALLATION PATH: '
-
-if(Test-Path -Path $path -PathType Leaf == False) {
-  echo "No installation could be found..."
-  pause
-  exit
-}
-
-vcpkg integrate install
-vcpkg install curl:x86-windows-static
-vcpkg install cpr:x86-windows-static
-vcpkg install jsoncpp
+echo "Installing needed dependencies for CLEO v5 build process"
+./vcpkg integrate install
+./vcpkg install curl:x86-windows-static
+./vcpkg install cpr:x86-windows-static
+./vcpkg install jsoncpp:x86-windows-static
+echo "Finished installing dependencies"
