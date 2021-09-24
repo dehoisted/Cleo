3c4331d561231a39e895769f48e5806153bdf5cb
orlando <75084509+dehoisted@users.noreply.github.com>
2021-09-24 18:15:13 -0400

    Update Includes.h

--- Cleo/Includes.h
+++ Cleo/Includes.h
@@ -1,11 +1,15 @@
-extern "C++" {
 #pragma once
+
+extern "C" {
+#include <intrin.h>
+}
+
+extern "C++" {
 //Uncomment the macros below if you get compilation errors. (though they should already be in the preprocessor according to the vs project)
 //#define _CRT_SECURE_NO_WARNINGS
 //#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
 //#define CURL_STATICLIB
 #include <regex>
-#include <intrin.h>
 #include "Modules.h" // Includes "Config.h"
 #include "User.h"
 #include "Anti.h"
